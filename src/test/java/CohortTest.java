8de4b2db9e9be776a16c6ad8be20d19f969070e5
Chris Davis <christopher.j.davis03@gmail.com>
2022-07-05 10:59:25 -0500

    eh

--- src/test/java/CohortTest.java
+++ src/test/java/CohortTest.java
@@ -1,7 +1,15 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class CohortTest {
-    @Before
     Cohort umbriel = new Cohort();
+
+    @Test
+    public void testAddStudent(){
+        umbriel.addStudent(new Student(1234567890,"Old Greg"));
+        assertNotNull(umbriel);
+    }
 
 }
